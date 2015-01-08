e4d42416094aaf849b34c9bbedc77d73f228a43b
sfaenza <cinzia.faenza@gmail.com>
2015-01-07 16:19:59 -0800

    created content datastream

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,13 +1,14 @@
 class Book < ActiveFedora::Base
-
-  # This is the include statement
   include Hydra::AccessControls::Permissions
-  
+
   has_metadata 'descMetadata', type: BookMetadata
 
   has_many :pages, :property => :is_part_of
 
-  has_attributes :title, datastream: 'descMetadata', multiple: false
-  has_attributes :author, datastream: 'descMetadata', multiple: false
+  # has_attributes :title, datastream: 'descMetadata', multiple: false
+  # has_attributes :author, datastream: 'descMetadata', multiple: false
+
+  delegate :title, to: 'descMetadata'
+  delegate :author, to: 'descMetadata'
 
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -6,4 +6,6 @@ class Page < ActiveFedora::Base
   has_attributes :number, datastream: 'descMetadata', multiple: false
   has_attributes :text, datastream: 'descMetadata', multiple: false
 
+  has_file_datastream "pageContent"
+
 end
